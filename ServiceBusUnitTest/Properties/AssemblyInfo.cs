1d8d5a7e78fbbe4bb1a6d909787e51e96adbaeac
Nitish Kumar <srinitishjha@gmail.com>
2019-12-29 16:59:18 +0530

    tst

---
+++ ServiceBusUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ServiceBusUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ServiceBusUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9d0c6a8a-c9b2-43ac-9bf5-3f7e3a994d30")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ServiceBusUnitTest/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServiceBusPOC;
+
+namespace ServiceBusUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CallSender()
+        {
+            Sender.Send().GetAwaiter().GetResult();
+        }
+
+        [TestMethod]
+        public void CallReceiver()
+        {
+            //Receiver.MainAsync().GetAwaiter().GetResult();
+            Receiver.Main();
+        }
+    }
+}
